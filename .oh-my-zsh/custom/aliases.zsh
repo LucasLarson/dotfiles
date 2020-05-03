b433236e25b8fa1d7ccc2637418e0f38b68a7e37
Lucas Larson <LucasLarson@riseup.net>
2020-05-03 11:32:09 -0400

    add `gu` for @ohmyzsh `gf && gsu && gst`

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
 alias gcm="git commit --message"
 alias gfgs="git fetch && git status"
+alias gu="git fetch && git submodule update && git status"
 alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update && git status"
