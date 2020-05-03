02c0c8cbf744fc8b8762c076776341d3216240a3
Lucas Larson <LucasLarson@riseup.net>
2020-05-03 13:51:58 -0400

    add `--init --recursive` to `gsu`

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
 alias gcm="git commit --message"
 alias gfgs="git fetch && git status"
-alias gu="git fetch && git submodule update && git status"
-alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update && git status"
+alias gu="git fetch && git submodule update --init --recursive && git status"
+alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update --init --recursive && git status"
