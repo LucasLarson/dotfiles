60f94aefa4886dafba64a759b90e0125377e7d9e
Lucas Larson <LucasLarson@riseup.net>
2020-05-14 17:19:53 -0400

    add `alias unixtime="date +%s"` to @ohmyzsh aliases
    via @Naresh https://stackoverflow.com/a/12312982
    where
    `date` is %c à la “Thu Mar  3 23:05:25 2005” https://github.com/coreutils/coreutils/blob/2ed7c2867974ccf7abc61c34ad7bf9565489c18e/src/date.c#L188
    `%s` is “seconds since 1970-01-01 00:00:00 UTC”
    https://github.com/coreutils/coreutils/blob/2ed7c2867974ccf7abc61c34ad7bf9565489c18e/src/date.c#L221

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,4 +2,5 @@ alias gcm="git commit --message"
 alias gfgs="git fetch && git status"
 alias gu="git fetch && git submodule update --init --recursive && git status"
 alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update --init --recursive && git status"
+alias unixtime="date +%s" # via @Naresh https://stackoverflow.com/a/12312982
 alias whihc="which"
