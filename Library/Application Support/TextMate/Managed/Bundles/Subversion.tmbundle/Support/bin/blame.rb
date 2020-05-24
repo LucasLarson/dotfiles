296b7b33618404c3622b3cf821dbc0e1a276e9ae
Lucas Larson <LucasLarson@riseup.net>
2020-05-24 14:16:34 -0400

    rm @TextMate’s Subversion bundle

--- Library/Application Support/TextMate/Managed/Bundles/Subversion.tmbundle/Support/bin/blame.rb
+++
@@ -1,8 +0,0 @@
-require "#{ENV['TM_SUPPORT_PATH']}/lib/exit_codes"
-require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/subversion"
-require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/view/blame_html"
-
-abort "The `blame` command only accepts one working copy path" if ARGV.size > 1
-base = Pathname.new(ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || Dir.pwd)
-Subversion::Blame::HTMLView.new(Subversion.blame(base, ARGV), ENV['TM_LINE_NUMBER'].to_i).render
-TextMate.exit_show_html
--- Library/Application Support/TextMate/Managed/Bundles/Subversion.tmbundle/Support/bin/resolved.rb
+++
@@ -1,7 +0,0 @@
-require "#{ENV['TM_SUPPORT_PATH']}/lib/textmate"
-require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/subversion"
-require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"
-
-unless ARGV.empty?
-  TextMate::UI.tool_tip("<strong>svn resolved</strong><p>#{htmlize Subversion.run("resolved", ARGV)}</p>", :format => :html)
-end
