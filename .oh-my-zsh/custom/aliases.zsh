b0ddf8c487631d49fc526f8faf563a4cda79890c
Lucas Larson <91468+LucasLarson@users.noreply.github.com>
2020-06-02 09:07:31 -0400

    replace w̸h̸c̸i̸h̸ with “which”

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,4 +3,5 @@ alias gfgs="git fetch && git status"
 alias gu="git fetch && git submodule update --init --recursive && git status"
 alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update --init --recursive && git status"
 alias unixtime="date +%s" # via @Naresh https://stackoverflow.com/a/12312982
+alias whcih="which"
 alias whihc="which"
