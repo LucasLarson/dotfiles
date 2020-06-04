6f15ddb65d5a32b1eea8bd4533532de5fb4dbb6c
Lucas Larson <LucasLarson@riseup.net>
2020-06-04 15:15:37 -0400

    mv g̸t̸i̸ → git alias to $ZSH_CUSTOM/aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -6,6 +6,7 @@ alias apm="apm-nightly"
 
 alias gcm="git commit --message"
 alias gfgs="git fetch && git status"
+alias gti="git"
 alias gu="git fetch && git submodule update --init --recursive && git status"
 alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update --init --recursive && git status"
 alias unixtime="date +%s" # via @Naresh https://stackoverflow.com/a/12312982
