ce740f4b75798b68f09f174a927c573aadeb02b5
Lucas Larson <LucasLarson@riseup.net>
2020-06-04 15:05:12 -0400

    add `alias vim="nvim"` for @Neovim instead of @Vim

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,5 +3,6 @@ alias gfgs="git fetch && git status"
 alias gu="git fetch && git submodule update --init --recursive && git status"
 alias mu="cd ~/Dropbox/Mackup && mackup backup && git fetch && git submodule update --init --recursive && git status"
 alias unixtime="date +%s" # via @Naresh https://stackoverflow.com/a/12312982
+alias vim="nvim"
 alias whcih="which"
 alias whihc="which"
