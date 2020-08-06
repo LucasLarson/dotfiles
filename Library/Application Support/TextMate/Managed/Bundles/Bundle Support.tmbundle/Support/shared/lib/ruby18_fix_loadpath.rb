0e56d01d36f829995b726c5cdf889e47ac701adb
Lucas Larson <LucasLarson@riseup.net>
2020-08-06 19:37:48 -0400

    replace local @TextMate Bundle Support.tmbundle with submodule
    https://github.com/textmate/bundle-support.tmbundle/commit/13169c6209ebb02564009c4ce573d62452c9b3ae

--- Library/Application Support/TextMate/Managed/Bundles/Bundle Support.tmbundle/Support/shared/lib/ruby18_fix_loadpath.rb
+++
@@ -1,2 +0,0 @@
-# This is used by bin/ruby18 to fix the hardcoded loadpath
-$:.each { |path| path.sub!(%r{^/Users/msheets/rubyinstalled}, "#{ENV['HOME']}/Library/Application Support/TextMate/Ruby/1.8.7") }
