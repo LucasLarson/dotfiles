96ed767aa2a58150b1c7c45bc23570d622d77492
Lucas Larson <LucasLarson@riseup.net>
2020-08-09 12:23:30 -0400

    replace local @TextMate LaTeX.tmbundle with submodule
    https://github.com/textmate/latex.tmbundle/commit/404e0049b67d54c2733b5a16eb600740c4ec2e19

--- Library/Application Support/TextMate/Managed/Bundles/LaTeX.tmbundle/Tests/bin/nosetests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# ------------------------------------------------------------------------------
-#           Run nose tests for all Python commands
-# ------------------------------------------------------------------------------
-
-export PATH=/Library/TeX/texbin:$PATH
-nosetests --with-doctest Support/lib/Python/*.py Support/bin/*.py
