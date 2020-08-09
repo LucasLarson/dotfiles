3ce79f638137d70515b90da5ff5f94468c66d5e7
Lucas Larson <LucasLarson@riseup.net>
2020-08-09 12:23:30 -0400

    replace local @TextMate LaTeX.tmbundle with submodule
    https://github.com/textmate/latex.tmbundle/commit/404e0049b67d54c2733b5a16eb600740c4ec2e19

--- Library/Application Support/TextMate/Managed/Bundles/LaTeX.tmbundle/Support/bin/label_completion.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-# -- Imports -------------------------------------------------------------------
-
-require ENV['TM_BUNDLE_SUPPORT'] + '/lib/Ruby/latex.rb'
-
-# -- Main ----------------------------------------------------------------------
-
-puts(LaTeX.label_names.join("\n"))
