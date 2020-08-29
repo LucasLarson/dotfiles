e37d8f673ac19734980de8da4cc664a1e0bbc6dc
Lucas Larson <LucasLarson@riseup.net>
2020-08-29 16:19:01 -0400

    replace local @TextMate R.tmbundle with submodule
    TextMate/R.tmbundle@5dc52fc

--- Library/Application Support/TextMate/Managed/Bundles/R.tmbundle/Support/bin/initCommandArgsDir.sh
+++
@@ -1,6 +0,0 @@
-WDIR="$HOME/Library/Application Support/TextMate/R/help"
-
-if [ ! -e "$WDIR/command_args" ]; then
-	mkdir -p "$WDIR/command_args"
-	cp -R "$TM_BUNDLE_SUPPORT"/lib/command_args "$WDIR"
-fi
