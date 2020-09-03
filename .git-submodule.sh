97ddf4960e35474dead5e8dcb21697a8ff4e9359
Lucas Larson <LucasLarson@riseup.net>
2020-09-03 10:12:34 -0400

    add double quotes to prevent globbing, splitting (SC2086)

--- .git-submodule.sh
+++ .git-submodule.sh
@@ -5,8 +5,8 @@
 # $ sh .git-submodule.sh
 git submodule init
 for i in $(git submodule | sed -e 's/.* //'); do
-    spath=$(git config -f .gitmodules --get submodule.$i.path)
-    surl=$(git config -f .gitmodules --get submodule.$i.url)
-    git clone --depth 1 $surl $spath
+    spath=$(git config -f .gitmodules --get submodule."$i".path)
+    surl=$(git config -f .gitmodules --get submodule."$i".url)
+    git clone --depth 1 "$surl" "$spath"
 done
 git submodule update
