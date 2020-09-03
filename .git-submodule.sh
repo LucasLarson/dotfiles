5e78064690f960927c7bf8ac109428bd8ed2cc6e
Lucas Larson <LucasLarson@riseup.net>
2020-09-03 10:16:02 -0400

    swap `git config`’s `-f` for `--file`

--- .git-submodule.sh
+++ .git-submodule.sh
@@ -5,8 +5,8 @@
 # $ sh .git-submodule.sh
 git submodule init
 for i in $(git submodule | sed -e 's/.* //'); do
-    spath=$(git config -f .gitmodules --get submodule."$i".path)
-    surl=$(git config -f .gitmodules --get submodule."$i".url)
+    spath=$(git config --file .gitmodules --get submodule."$i".path)
+    surl=$(git config --file .gitmodules --get submodule."$i".url)
     git clone --depth 1 "$surl" "$spath"
 done
 git submodule update
