d6fe90ea21eeb4dadb3f79d1f8f2bb19d22ed415
Lucas Larson <LucasLarson@riseup.net>
2020-10-04 22:30:36 -0400

    add vertical space to shallow gitmodules shell script

--- .git-submodule.sh
+++ .git-submodule.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
+#
+#
+#
 # Shallow .gitmodules submodule installations
 # Mauricio Scheffer https://stackoverflow.com/a/2169914
+#
+#
 # place in the top of a Git repository and invoke with
 # $ sh .git-submodule.sh
+
+
 git submodule init
 for i in $(git submodule | sed -e 's/.* //'); do
     spath=$(git config --file .gitmodules --get submodule."$i".path)
