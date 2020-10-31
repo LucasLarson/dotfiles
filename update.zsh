d285b0d0da435aabddb4a7094b791812773962bc
Lucas Larson <LucasLarson@riseup.net>
2020-10-31 11:56:40 -0400

    combine some crossplatform instructions

--- update.zsh
+++ update.zsh
@@ -3,10 +3,11 @@
 update=1
 clear && clear
 
-if [[ Darwin == "$(uname)" ]]; then
 
 printf '                 .___       __\n __ ________   __\x7c _\x2f____ _\x2f  \x7c_  ____\n\x7c  \x7c  \x5c____ \x5c \x2f __ \x7c\x5c__  \x5c\x5c   __\x5c\x2f __ \x5c\n\x7c  \x7c  \x2f  \x7c_\x3e \x3e \x2f_\x2f \x7c \x2f __ \x5c\x7c  \x7c \x5c  ___\x2f\n\x7c____\x2f\x7c   __\x2f\x5c____ \x7c\x28____  \x2f__\x7c  \x5c___  \x3e\n      \x7c__\x7c        \x5c\x2f     \x5c\x2f          \x5c\x2f\n a Lucas Larson production\n\n'
 sleep 1.0
+
+if [[ Darwin == "$(uname)" ]]; then
 
 printf '\n\xf0\x9f\x93\xa1 verifying network connectivity...\n'
 sleep 0.5
