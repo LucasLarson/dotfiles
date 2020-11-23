67338bb715dc4d3a158e5135d67975ea2b2cbc8a
Lucas Larson <LucasLarson@riseup.net>
2020-11-23 11:17:14 -0500

    allow `git verify-commit`’s alias to allow a commit hash as argument

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -139,8 +139,12 @@ gu () {
 }
 
 # https://github.com/tarunsk/dotfiles/blob/5b31fd6/.always_forget.txt#L1957
-alias gvc="git verify-commit HEAD"
-
+# alias gvc="git verify-commit HEAD"
+gvc () {(
+  # if there is an argument (commit hash), use it
+  # otherwise check `HEAD`
+  git verify-commit "${1:-HEAD}"
+)}
 
 # GPG
 if command -v gpg2 > /dev/null 2>&1; then
