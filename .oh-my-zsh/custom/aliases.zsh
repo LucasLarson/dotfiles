3bdf8778c5d188489f9bff1040723b9477f8f395
Lucas Larson <LucasLarson@riseup.net>
2020-11-27 17:14:50 -0500

    rm unnecessary parentheses from `gmm` function to `git merge` the default branch
    it has no leakable variables

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -105,13 +105,13 @@ alias gdb="git_default_branch"
 alias gmc="GIT_MERGE_VERBOSITY=4 git merge --continue"
 
 # git merge main
-gmm () {(
+gmm () {
   # set Git merge verbosity environment variable
   # 4 “shows all paths as they are processed” but
   # 5 is “show detailed debugging information”
   # https://github.com/progit/progit2/commit/aea93a7
   GIT_MERGE_VERBOSITY=4 git merge --verbose --progress --strategy-option patience "$(git_default_branch)"
-)}
+}
 
 alias gmv="git mv --verbose"
 
