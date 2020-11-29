569ff83a0184c93e12bdc63ee7361dd36df88306
Lucas Larson <91468+LucasLarson@users.noreply.github.com>
2020-11-29 09:53:20 -0500

    add attribution link for shell packages

--- init.sh
+++ init.sh
@@ -18,6 +18,7 @@ apk add curl curl-doc python2 python2-doc python3 python3-doc
 # https://wiki.alpinelinux.org/w/index.php?oldid=17773&title=How_to_get_regular_stuff_working
 apk add mandoc mandoc-doc man-pages less less-doc
 
+# https://wiki.alpinelinux.org/w/index.php?oldid=17773&title=How_to_get_regular_stuff_working#Shell_.40_commandline
 apk add util-linux util-linux-doc pciutils pciutils-doc usbutils usbutils-doc coreutils coreutils-doc binutils binutils-doc findutils findutils-doc grep grep-doc
 
 # SSH
