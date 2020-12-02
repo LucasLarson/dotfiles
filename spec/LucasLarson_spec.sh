a7f2cd38767e3ce512e255de1ebd7469ec9dfac1
Lucas Larson <91468+LucasLarson@users.noreply.github.com>
2020-12-02 07:52:10 -0500

    conform to shfmt

--- spec/LucasLarson_spec.sh
+++ spec/LucasLarson_spec.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 Describe "Sample specfile"
-  Describe "hello()"
-    hello() {
-      echo # "hello $1"
-    }
+Describe "hello()"
+hello() {
+  echo # "hello $1"
+}
 
-    It "puts greeting, but not implemented"
-      Pending "You should implement hello function"
-      When call hello world
-      The output should eq "hello world"
-    End
-  End
+It "puts greeting, but not implemented"
+Pending "You should implement hello function"
+When call hello world
+The output should eq "hello world"
+End
+End
 End
