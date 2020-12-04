f23b5c842832b853c1abf603b13d5d5b01ea0d5f
Lucas Larson <LucasLarson@riseup.net>
2020-12-04 16:53:15 -0500

    add `make` and `cmake`

--- init.sh
+++ init.sh
@@ -19,6 +19,9 @@ apk add mandoc mandoc-doc man-pages less less-doc
 # https://wiki.alpinelinux.org/w/index.php?oldid=17773&title=How_to_get_regular_stuff_working#Shell_.40_commandline
 apk add util-linux util-linux-doc pciutils pciutils-doc usbutils usbutils-doc coreutils coreutils-doc binutils binutils-doc findutils findutils-doc grep grep-doc
 
+# make, cmake
+apk add make make-doc cmake cmake-doc
+
 # ssh
 # https://wiki.alpinelinux.org/w/index.php?oldid=13842&title=Setting_up_a_ssh-server#OpenSSH
 # https://wiki.alpinelinux.org/w/index.php?oldid=17295&title=Setting_up_a_laptop#Creating_GPG_keys
