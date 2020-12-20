8733b6f020e8e0fa5c80b31f74ccfce74eaba445
Lucas Larson <LucasLarson@riseup.net>
2020-12-19 19:16:37 -0500

    add `fdf` to find duplicate files via @LinuxJournal
    but swapping `md5sum` for `sha512sum`

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -213,6 +213,12 @@ cleanup () {(
   $verbose -delete
 )}
 
+# find duplicate files
+# https://linuxjournal.com/content/boost-productivity-bash-tips-and-tricks
+fdf () {(
+  find -- . -not -empty -type f -printf '%s\n' | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 sha512sum | sort | uniq -w32 --all-repeated=separate
+)}
+
 # https://unix.stackexchange.com/a/30950
 alias mv="mv -v -i"
 
