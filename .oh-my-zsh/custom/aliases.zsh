5db9bd4f930e5c4a4c2c3c30b0fe28cc602ef25d
Lucas Larson <LucasLarson@riseup.net>
2021-01-05 11:13:31 -0500

    skip removal of `.keep` files during cleanup
    https://github.com/emscripten-core/emscripten/blob/ceda5ca4a44b4e9133175e2b97e29ab5d0d28a8d/tests/third_party/enet/m4/.keep

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -202,6 +202,7 @@ cleanup () {(
   find -- . -type f -size 0 \( \
     -not -path '*/.git/*' -and \
     -not -name '*.gitkeep' -and \
+    -not -name '*.keep' -and \
     -not -name '*hushlogin' -and \
     -not -name '*LOCK' -and \
     -not -name '*lock' -and \
