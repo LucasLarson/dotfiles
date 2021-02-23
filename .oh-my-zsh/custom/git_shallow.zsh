e123afaf68f5c22a02792d96adc901b14950b30b
Lucas Larson <LucasLarson@riseup.net>
2021-02-23 17:13:08 -0500

    surround variable references with braces (SC2250)

--- .oh-my-zsh/custom/git_shallow.zsh
+++ .oh-my-zsh/custom/git_shallow.zsh
@@ -8,9 +8,9 @@
 git_shallow() {
   git submodule init
   for i in $(git submodule | sed -e 's/.* //'); do
-    submodule_path=$(git config --file .gitmodules --get submodule."$i".path)
-    submodule_url=$(git config --file .gitmodules --get submodule."$i".url)
-    git clone --depth 1 --shallow-submodules "$submodule_url" "$submodule_path"
+    submodule_path=$(git config --file .gitmodules --get submodule."${i}".path)
+    submodule_url=$(git config --file .gitmodules --get submodule."${i}".url)
+    git clone --depth 1 --shallow-submodules "${submodule_url}" "${submodule_path}"
   done
   git submodule update
 }
