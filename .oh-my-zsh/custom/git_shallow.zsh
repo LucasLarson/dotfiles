d960de495635d0689a8808f1cc01fd0f62598652
Lucas Larson <LucasLarson@riseup.net>
2021-02-23 17:06:17 -0500

    move executable script into function

--- .oh-my-zsh/custom/git_shallow.zsh
+++ .oh-my-zsh/custom/git_shallow.zsh
@@ -5,10 +5,12 @@
 # Shallow .gitmodules submodule installations
 # Mauricio Scheffer https://stackoverflow.com/a/2169914
 
-git submodule init
-for i in $(git submodule | sed -e 's/.* //'); do
-  spath=$(git config --file .gitmodules --get submodule."$i".path)
-  surl=$(git config --file .gitmodules --get submodule."$i".url)
-  git clone --depth 1 "$surl" "$spath"
-done
-git submodule update
+git_shallow() {
+  git submodule init
+  for i in $(git submodule | sed -e 's/.* //'); do
+    spath=$(git config --file .gitmodules --get submodule."$i".path)
+    surl=$(git config --file .gitmodules --get submodule."$i".url)
+    git clone --depth 1 "$surl" "$spath"
+  done
+  git submodule update
+}
