0bf14b13c75259eb8d39af6a4ccf08818278f969
Lucas Larson <LucasLarson@riseup.net>
2021-03-27 12:48:27 -0400

    make `same_file` function work for symlinks, too

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -521,11 +521,10 @@ alias mv='mv -v -i'
 same_file() {
   # POSIX-compliant conditional expression `-ef`
   # to find if two files refer to the device and inode numbers
-  # same_file file1 file2
+  # usage: same_file file1 file2
   # return 0 if they match
   # https://superuser.com/a/196655
-  set -x
-  [ "$(stat -c %d:%i "$1")" = "$(stat -c %d:%i "$2")" ] && return 0 || return 1
+  [ "$(stat -L -c %d:%i "$1")" = "$(stat -L -c %d:%i "$2")" ] && return 0 || return 1
 }
 
 # https://github.com/mcornella/dotfiles/commit/ff4e527
