5ef7d2dbd2a6908eea56e8c8d18b4cc37b7f2831
Lucas Larson <LucasLarson@riseup.net>
2021-04-11 18:18:23 -0400

    set shallow submodule cloning to `--sparse`

--- custom/git_shallow.zsh
+++ custom/git_shallow.zsh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 #
-#
-#
 # Shallow .gitmodules submodule installations
 # Mauricio Scheffer https://stackoverflow.com/a/2169914
 
@@ -10,7 +8,7 @@ git_shallow() {
   for i in $(git submodule | sed -e 's/.* //'); do
     submodule_path=$(git config --file .gitmodules --get submodule."${i}".path)
     submodule_url=$(git config --file .gitmodules --get submodule."${i}".url)
-    git clone --depth 1 --shallow-submodules "${submodule_url}" "${submodule_path}"
+    git clone --depth 1 --shallow-submodules --sparse "${submodule_url}" "${submodule_path}"
   done
   git submodule update
 }
