45b9dcada63ade61d0a5645ae9c25bbcd6f2e6a0
Lucas Larson <LucasLarson@riseup.net>
2021-04-16 16:40:50 -0400

    insert non-breaking space between space and emoji

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -103,7 +103,7 @@ git_commit_initial_commit() {
   # if there are non-repository files present, then add them and commit
   if [ -n "$(git ls-files --others)" ]; then
     git add -- . &&
-      git commit --verbose --message "$(printf '\xe2\x9c\xa8 initial commit')"
+      git commit --verbose --message "$(printf '\xe2\x9c\xa8\xc2\xa0 initial commit')"
   fi
 }
 alias gcic='git_commit_initial_commit'
