ff8c39faa2d687b8e1763465f3c25853614b2259
Lucas Larson <LucasLarson@riseup.net>
2021-05-31 20:46:08 -0400

    start script from `$HOME`

--- setup/linux/alpine/init.sh
+++ setup/linux/alpine/init.sh
@@ -13,6 +13,9 @@ sleep 1
 printf '  a Lucas Larson production\n\n'
 sleep 1
 
+# start from `$HOME`
+[ -n "${HOME}" ] && command cd -- "${HOME}" || exit 1
+
 # unset `$PS4`
 # if this quaternary prompt string is already unset, then
 # set it to the POSIX default: `+ `
