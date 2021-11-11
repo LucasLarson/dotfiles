e14861dac73fb45ede599266a8159eee87955cb5
Lucas Larson <LucasLarson@riseup.net>
2021-11-11 14:18:37 -0500

    absorb `git_shallow` file into amalgamated aliases file

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -255,6 +255,22 @@ git_restore() {
   unset file
 }
 alias grs='git_restore'
+
+git_shallow() {
+  # Shallow .gitmodules submodule installations
+  # Mauricio Scheffer https://stackoverflow.com/a/2169914
+
+  command git submodule init
+  for submodule in $(command git submodule | command sed -e 's/.* //'); do
+    submodule_path="$(command git config --file .gitmodules --get submodule."${submodule}".path)"
+    submodule_url="$(command git config --file .gitmodules --get submodule."${submodule}".url)"
+    command git clone --depth=1 --shallow-submodules "${submodule_url}" "${submodule_path}"
+  done
+  command git submodule update
+
+  unset submodule submodule_path submodule_url
+}
+
 alias gs='command git status'
 
 git_submodule_update() {
