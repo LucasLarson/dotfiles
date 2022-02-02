eab8aa389c2151cbd7bf86e7ffab4dcf34d021f5
Lucas Larson <LucasLarson@riseup.net>
2022-02-01 19:26:36 -0500

    remove proprietary @OhMyZsh reference

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -8,8 +8,8 @@
 # https://zsh.sourceforge.io/Doc/Release/Shell-Grammar.html#index-exec
 alias ,='. "${HOME-}"/."${SHELL##*[-./]}"rc && exec -l -- "${SHELL##*[-./]}"'
 aliases() {
-  ${EDITOR:-vi} -- "${ZSH_CUSTOM:="${DOTFILES-}"/custom}"/aliases."${SHELL##*[-./]}"
-  "$(command -v .)" "${ZSH_CUSTOM-}"/aliases."${SHELL##*[-./]}"
+  ${EDITOR:-vi} -- "${DOTFILES-}/custom/aliases.${SHELL##*[-./]}"
+  "$(command -v .)" "${DOTFILES-}/custom/aliases.${SHELL##*[-./]}"
 }
 
 # Atom
