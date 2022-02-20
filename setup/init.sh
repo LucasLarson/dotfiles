8a24e5e4b9d30b1f1d701982a949ed6e8fca7c0e
Lucas Larson <LucasLarson@riseup.net>
2022-02-20 16:02:36 -0500

    copy findutils documentation for `find --version`
    findutils v4.9.0
    ╭───────┬─────────────────╮
    │ count │      usage      │
    ├───────┼─────────────────┤
    │     7 │ find --version  │
    │     0 │ find -version   │
    ╰───────┴─────────────────╯
    source: https://git.savannah.gnu.org/cgit/findutils.git/tree/?h=v4.9.0
    
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- setup/init.sh
+++ setup/init.sh
@@ -115,7 +115,7 @@ command -v less >/dev/null 2>&1 || {
 # https://wiki.alpinelinux.org/w/index.php?oldid=18038&title=Alpine_newbie_apk_packages#coreutils_libc_and_utmps_in_alpine
 install coreutils coreutils-doc
 { test -x '/usr/bin/coreutils' &&
-  test "$(command find -version 2>/dev/null | command head -n 1 | command awk '{print $3}' | command tr -d '()')" = 'findutils'; } || {
+  test "$(command find --version 2>/dev/null | command head -n 1 | command awk '{print $3}' | command tr -d '()')" = 'findutils'; } || {
   { set +x; } 2>/dev/null
   printf 'installing Linux utilities...\n' 2>/dev/null
   set -x
