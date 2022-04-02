cde2432a6dab64311006b8406fa40511137144bc
Lucas Larson <LucasLarson@riseup.net>
2022-04-02 11:02:41 -0400

    add `file_closes_with_newline` function to fail if target lacks it
    @FelipeC: https://stackoverflow.com/revisions/2421790/4
    
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -585,6 +585,14 @@ filename_spaces_to_underscores() {
       done
   )
 }
+
+file_closes_with_newline() {
+  set -o nounset
+  test "$(command tail -c 1 -- "${1-}" | command wc -l)" -eq '0' &&
+    return 1
+  set +o nounset
+}
+
 command -v fd >/dev/null 2>&1 &&
   alias fd='command fd --hidden'
 
