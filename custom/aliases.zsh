2438e7935b8995b835a8e9e41176dc9cedf5d85e
Lucas Larson <LucasLarson@riseup.net>
2022-10-07 19:30:31 -0400

    set `open pb` to open POSIX specification’s builtins
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1460,8 +1460,11 @@ open() {
     command open -- .
   else
     case "${1-}" in
-    p)
+    p | posix_utilities)
       command open -- 'https://opengroup.org/onlinepubs/9699919799/idx/utilities.html'
+      ;;
+    pb | posix_builtins)
+      command open -- 'https://opengroup.org/onlinepubs/9699919799/idx/sbi.html'
       ;;
     sc*)
       command open -- 'https://shellcheck.net/wiki/SC'"${1#sc}"
