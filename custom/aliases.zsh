a8d7e5784efbf68806c7ef6a13991e784c8850d3
Lucas Larson <LucasLarson@riseup.net>
2022-10-25 14:33:42 -0400

    if GitHub `gh` is installed, `ghs` returns `gh status`
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1263,6 +1263,9 @@ gvc() {
   command git verify-commit "${1:-HEAD}"
 }
 
+command -v -- gh >/dev/null 2>&1 &&
+  alias ghs='command gh status'
+
 gitlab_create_repository() {
   command git push --set-upstream git@gitlab.com:"${GITLAB_USERNAME:-${USER-}}"/"$(
     command git rev-parse --show-toplevel |
