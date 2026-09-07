e74f245db9f82f94bb20a2cd706aeb6a80de002b
Lucas Larson <LucasLarson@riseup.net>
2026-09-07 03:43:14 -0400

    refactor: prefer function `set -x` to `set -v`
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- custom/aliases.sh
+++ custom/aliases.sh
@@ -4860,7 +4860,7 @@ alias \
 
 git_submodule_cleanup() {
   set \
-    -o verbose
+    -o xtrace
   while test "$(
     find -- . \
       '(' \
@@ -4893,7 +4893,7 @@ git_submodule_cleanup() {
     git_update "${@-}"
   {
     set \
-      +o verbose
+      +o xtrace
   } 2>/dev/null
 }
 alias gsc='git_submodule_cleanup'
