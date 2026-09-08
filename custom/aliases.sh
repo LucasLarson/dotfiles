9c3dd4e8816e985142a70c06b4dc0ef6a1b49aa0
Lucas Larson <LucasLarson@riseup.net>
2026-09-08 02:08:43 -0400

    docs: describe copy-to-clipboard aliases
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- custom/aliases.sh
+++ custom/aliases.sh
@@ -9344,7 +9344,7 @@ zshoptions_search() {
     } >/dev/null 2>&1
 }
 
-## zero-width space
+## zero-width space, shrug, and sparkle
 # copy to macOS clipboard
 alias \
   zwsp='printf -- '\''​'\'' | pbcopy' \
